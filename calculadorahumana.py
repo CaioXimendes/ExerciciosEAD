d0ff385e331b908dbd17cab80b1cc9e3356fc7a6
Caio Ximendes <128853913+CaioXimendes@users.noreply.github.com>
2024-01-23 22:44:19 -0300

    calculadora de divisao no papel em python
    correção de erros da calculadora de divisão

--- calculadorahumana.py
+++ calculadorahumana.py
@@ -6,16 +6,39 @@ quociente = float
 resto = float
 valordivisao = float
 variavel = 0
-quocientevisual = [0.0,0.0]
+quocientevisual = [0,0]
 
 divisor = float(input("Olá, informe o valor do divisor: "))
 dividendo = float(input("Certo, agora informe o valor do dividendo: "))
 
 if divisor > dividendo:
-    quociente = 0.0
+    quociente = 0
     quocientevisual[0] = quociente
+    valordivisao = quociente*divisor
+    resto = dividendo - valordivisao
+    if resto < divisor:
+        variavel = resto*10
+        quocientevisual [1] = ","     
+        print(f"{dividendo}|___{divisor}___")
+        print(f"{valordivisao}     {quocientevisual}")
+        print(f"{variavel}")
+        
+        for k in range(100):
+            if quociente*divisor > variavel:
+                quociente-=1
+                valordivisao = quociente*divisor
+                variavel = variavel - valordivisao
+                variavel = variavel*10
+                quocientevisual.append(quociente)
+                print(f"{dividendo}|___{divisor}___")
+                print(f"{valordivisao}     {quocientevisual}")
+                print(f"{variavel}")
+                quociente=quociente-k
+            else:
+                quociente+=1
+    
 elif divisor < dividendo:
-    quociente = 1.0
+    quociente = 1
     quocientevisual[0] = quociente
     if dividendo%divisor == 0:
         for y in range(100):
@@ -30,32 +53,37 @@ elif divisor < dividendo:
                 print(f"{resto}")
                 exit()
                 break
-
-valordivisao = quociente*divisor
-resto = dividendo - valordivisao
-
-if resto < divisor and dividendo%divisor != 0:
-    variavel = resto*10
-    quocientevisual [1] = ","     
-    print(f"{dividendo}|___{divisor}___")
-    print(f"{valordivisao}     {quocientevisual}")
-    print(f"{variavel}")
-elif resto == 0 and ponteiro == 0:
-    print(f"{dividendo}|___{divisor}___")
-    print(f"{valordivisao}     {quocientevisual}")
-    print(f"{variavel}")
-    
-    
-for x in range(100):
-    if quociente*divisor > variavel:
-        quociente-=1
-        valordivisao = quociente*divisor
-        variavel = variavel - valordivisao
-        variavel = variavel*10
-        quocientevisual.append(quociente)
-        print(f"{dividendo}|___{divisor}___")
-        print(f"{valordivisao}     {quocientevisual}")
-        print(f"{variavel}")
-        quociente=quociente-x
     else:
-        quociente+=1
+        for z in range(100):
+            if quociente*divisor < dividendo:
+                quociente+=1
+            else:
+                quociente-=1
+                valordivisao = quociente*divisor
+                resto = dividendo - valordivisao
+                quocientevisual[0] = quociente
+                break
+        if resto < divisor:
+            variavel = resto*10
+            quocientevisual [1] = ","     
+            print(f"{dividendo}|___{divisor}___")
+            print(f"{valordivisao}     {quocientevisual}")
+            print(f"{variavel}")
+            quociente=0
+        #elif resto == 0:
+            #print(f"{dividendo}|___{divisor}___")
+            #print(f"{valordivisao}     {quocientevisual}")
+            #print(f"{variavel}")
+            for x in range(100):
+                if quociente*divisor > variavel:
+                    quociente-=1
+                    valordivisao = quociente*divisor
+                    variavel = variavel - valordivisao
+                    quocientevisual.append(quociente)
+                    print(f"{dividendo}|___{divisor}___")
+                    print(f"{valordivisao}     {quocientevisual}")
+                    print(f"{variavel}")
+                    quociente=quociente-x
+                    variavel = variavel*10
+                else:
+                    quociente+=1
